25905272967fbfbb7b370900151905c4d89e910a
brunoms@dcomp.ufs.br <b2*Fox7997>
2023-09-16 09:47:14 -0300

    teste de criacao form

---
+++ src/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Form msg="GSearch"/>
+</template>
+
+<script>
+import Form from './components/SearchForm.vue'
+
+export default {
+  //name: 'App',
+  name: 'SForm',
+  components: {
+    Form
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+/*
 import App from './App.vue'
 
 createApp(App).mount('#app')
+*/
+import Search from './Search.vue'
+
+createApp(Search).mount('#app')
+createApp(Search).mount('#search')
